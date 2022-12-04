a37d478340e9a0e0ad8a49e22a5a2948de5896d4
Tantriii <92931872+Tantriii@users.noreply.github.com>
2022-12-04 15:28:25 +0700

    Update Laptop.java

--- TPMODUL2_TANTRI/src/Laptop.java
+++ TPMODUL2_TANTRI/src/Laptop.java
@@ -15,16 +15,16 @@ public class Laptop extends Perangkat {
         System.out.println("Laptop ini memiliki drive tipe" + " "+ drive + " "+ "dengan ram sebesar" + " "+ ram + "GB dan processor secepat" + " " + processor + " "+"GHZ. Selain itu Laptop ini" + (webcam == false ? "memiliki " : " juga memiliki ") + "webcam");
     }
     
-    // method void bukaGame(String nama_game)
+    // method void bukaGame
     public void bukaGame(String nama_game){
         System.out.println("Laptop berhasil membuka" +" "+ nama_game);
     }
-     // method void kirimEmail(String email)
+     // method void kirimEmail
      public void kirimEmail(String email){
         System.out.println("Laptop berhasil mengirim email ke" +" " + email);
     }
 
-    // method void kirimEmail(String email1, String email2)
+    // method void kirimEmail
     public void kirimEmail(String email1, String email2){
         System.out.println("Laptop berhasil mengirim email ke" + " " + email1 + "dan ke" +" "+ email2 );
     }
